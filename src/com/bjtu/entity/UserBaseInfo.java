51bd86a6f67a280d55cf1dd9153dfd52d3de4016
heweivp <1012867554@qq.com>
2017-05-05 16:39:43 +0800

    Add files via upload

---
+++ src/com/bjtu/entity/UserBaseInfo.java
@@ -0,0 +1,84 @@
+package com.bjtu.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 用户基本信息
+ * */
+@Entity
+@Table(name="UserBaseInfo")
+public class UserBaseInfo {
+	
+	@Id
+	private int user_id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String id_card;
+	
+	@Column
+	private String city;
+	
+	@Column
+	private String address;
+	
+	@Column
+	private String pic_url;
+
+	public int getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId_card() {
+		return id_card;
+	}
+
+	public void setId_card(String id_card) {
+		this.id_card = id_card;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPic_url() {
+		return pic_url;
+	}
+
+	public void setPic_url(String pic_url) {
+		this.pic_url = pic_url;
+	}
+	
+	
+}
