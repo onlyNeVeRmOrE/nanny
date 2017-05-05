cade8bd1cbbbd2d465a9c08a7300b30a72cfd105
heweivp <1012867554@qq.com>
2017-05-05 16:40:13 +0800

    Add files via upload

--- src/com/bjtu/service/NannyUserService.java
+++ src/com/bjtu/service/NannyUserService.java
@@ -3,6 +3,7 @@ package com.bjtu.service;
 import java.util.List;
 
 import com.bjtu.entity.NannyUser;
+import com.bjtu.entity.UserBaseInfo;
 
 public interface NannyUserService {
 	public List<NannyUser> getAllUser();
@@ -11,4 +12,10 @@ public interface NannyUserService {
     
     //注册
     public void register(String username,String password);
+    
+    //添加用户基本信息
+    public void addBaseInof(UserBaseInfo userBaseInfo);
+    
+    //根据username获取id
+    public int getIdByUsername(String username);
 }
